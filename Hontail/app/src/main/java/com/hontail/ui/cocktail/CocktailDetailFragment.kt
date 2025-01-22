ce0727b89eed9af0b34a6b1bdee04dc4ff5781e3
DESKTOP-PHKCE21\SSAFY <charlie216891@gmail.com>
2025-01-22 16:26:05 +0900

    [Feat] Add CocktailDetailFragment, CocktailCommentBottomSheetFragment, LoginFragment #front

---
+++ Hontail/app/src/main/java/com/hontail/ui/cocktail/CocktailDetailFragment.kt
@@ -0,0 +1,12 @@
+package com.hontail.ui.cocktail
+
+import com.hontail.R
+import com.hontail.base.BaseFragment
+import com.hontail.databinding.FragmentCocktailDetailBinding
+
+class CocktailDetailFragment : BaseFragment<FragmentCocktailDetailBinding>(
+    FragmentCocktailDetailBinding::bind,
+    R.layout.fragment_cocktail_detail
+) {
+
+}
---
+++ Hontail/app/src/main/java/com/hontail/ui/login/LoginFragment.kt
@@ -0,0 +1,18 @@
+package com.hontail.ui.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hontail.R
+import com.hontail.base.BaseFragment
+import com.hontail.databinding.FragmentIngredientAddBinding
+import com.hontail.databinding.FragmentLoginBinding
+
+class LoginFragment : BaseFragment<FragmentLoginBinding>(
+    FragmentLoginBinding::bind,
+    R.layout.fragment_login
+) {
+
+}
