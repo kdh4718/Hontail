40786dbe2ddaecf8e3d9898842487c298e9017df
DESKTOP-PHKCE21\SSAFY <charlie216891@gmail.com>
2025-01-22 16:10:36 +0900

    [Feat] Add IngredientListFragment, IngredientAddFragment #front

---
+++ Hontail/app/src/main/java/com/hontail/ui/ingredient/IngredientAddFragment.kt
@@ -0,0 +1,12 @@
+package com.hontail.ui.ingredient
+
+import com.hontail.R
+import com.hontail.base.BaseFragment
+import com.hontail.databinding.FragmentIngredientAddBinding
+
+class IngredientAddFragment : BaseFragment<FragmentIngredientAddBinding>(
+    FragmentIngredientAddBinding::bind,
+    R.layout.fragment_ingredient_add
+) {
+
+}
---
+++ Hontail/app/src/main/java/com/hontail/ui/ingredient/IngredientListFragment.kt
@@ -0,0 +1,12 @@
+package com.hontail.ui.ingredient
+
+import com.hontail.R
+import com.hontail.base.BaseFragment
+import com.hontail.databinding.FragmentIngredientListBinding
+
+class IngredientListFragment : BaseFragment<FragmentIngredientListBinding>(
+    FragmentIngredientListBinding::bind,
+    R.layout.fragment_ingredient_list
+) {
+
+}
