9505ca4568f1fc6bc1e7f1b3b62cc8e40b454d89
DESKTOP-PHKCE21\SSAFY <charlie216891@gmail.com>
2025-01-22 13:06:12 +0900

    [Feat] Add HomeFragment #front

---
+++ Hontail/app/src/main/java/com/hontail/ui/home/HomeFragment.kt
@@ -0,0 +1,12 @@
+package com.hontail.ui.home
+
+import com.hontail.R
+import com.hontail.base.BaseFragment
+import com.hontail.databinding.FragmentHomeBinding
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(
+    FragmentHomeBinding::bind,
+    R.layout.fragment_home
+) {
+
+}
--- Hontail/app/src/main/java/com/hontail/ui/mypage/MyPageFragment.kt
+++ Hontail/app/src/main/java/com/hontail/ui/mypage/MyPageFragment.kt
@@ -1,13 +1,13 @@
 package com.hontail.ui.mypage
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.hontail.R
+import com.hontail.base.BaseFragment
+import com.hontail.databinding.FragmentMyPageBinding
 
 
-class MyPageFragment : Fragment() {
+class MyPageFragment : BaseFragment<FragmentMyPageBinding>(
+    FragmentMyPageBinding::bind,
+    R.layout.fragment_my_page
+) {
 
 }
