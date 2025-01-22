ebb92c5cf959775eb19721d4a99744ac0f556565
DESKTOP-PHKCE21\SSAFY <charlie216891@gmail.com>
2025-01-22 16:39:07 +0900

    [Feat] Add CustomCocktailModifyFragment #front

---
+++ Hontail/app/src/main/java/com/hontail/ui/custom/CustomCocktailModifyFragment.kt
@@ -0,0 +1,18 @@
+package com.hontail.ui.custom
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hontail.R
+import com.hontail.base.BaseFragment
+import com.hontail.databinding.FragmentCustomCocktailBinding
+import com.hontail.databinding.FragmentCustomCocktailModifyBinding
+
+class CustomCocktailModifyFragment : BaseFragment<FragmentCustomCocktailModifyBinding>(
+    FragmentCustomCocktailModifyBinding::bind,
+    R.layout.fragment_custom_cocktail_modify
+) {
+
+}
