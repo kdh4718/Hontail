40fc1c6dbd70e05b94f7c27a5c63ddf91788888a
DESKTOP-PHKCE21\SSAFY <charlie216891@gmail.com>
2025-01-22 14:36:51 +0900

    [Feat] Add MyPageModifyFragment #front

---
+++ Hontail/app/src/main/java/com/hontail/ui/mypage/MyPageModifyFragment.kt
@@ -0,0 +1,12 @@
+package com.hontail.ui.mypage
+
+import com.hontail.R
+import com.hontail.base.BaseFragment
+import com.hontail.databinding.FragmentMyPageModifyBinding
+
+class MyPageModifyFragment : BaseFragment<FragmentMyPageModifyBinding>(
+    FragmentMyPageModifyBinding::bind,
+    R.layout.fragment_my_page_modify
+) {
+
+}
