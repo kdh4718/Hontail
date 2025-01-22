95e0e8236f8d5b279ce64d84168ca3905a4cbf50
DESKTOP-PHKCE21\SSAFY <charlie216891@gmail.com>
2025-01-22 15:21:05 +0900

    [Feat] Add CocktailRecipeFragment #front

---
+++ Hontail/app/src/main/java/com/hontail/ui/cocktail/CocktailRecipeFragment.kt
@@ -0,0 +1,18 @@
+package com.hontail.ui.cocktail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hontail.R
+import com.hontail.base.BaseFragment
+import com.hontail.databinding.FragmentCocktailRecipeBinding
+import com.hontail.databinding.FragmentZzimBinding
+
+class CocktailRecipeFragment : BaseFragment<FragmentCocktailRecipeBinding>(
+    FragmentCocktailRecipeBinding::bind,
+    R.layout.fragment_cocktail_recipe
+) {
+
+}
