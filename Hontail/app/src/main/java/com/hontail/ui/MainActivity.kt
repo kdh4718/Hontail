3f362311942e78a7320b48c8eb438468c8151aa7
charlie216891 <charlie216891@gmail.com>
2025-01-22 21:11:56 +0900

    [Feat] Status Bar Full Screen Setting #front

--- Hontail/app/src/main/java/com/hontail/ui/MainActivity.kt
+++ Hontail/app/src/main/java/com/hontail/ui/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.hontail.ui
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,5 +16,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        window.statusBarColor = Color.TRANSPARENT
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
     }
 }
