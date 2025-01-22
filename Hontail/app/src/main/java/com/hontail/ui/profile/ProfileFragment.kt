478da46413d0abaa301b0edc28b935161a01c0d4
DESKTOP-PHKCE21\SSAFY <charlie216891@gmail.com>
2025-01-22 15:18:00 +0900

    [Feat] Add ProfileFragment, ZzimFragment #front

---
+++ Hontail/app/src/main/java/com/hontail/ui/profile/ProfileFragment.kt
@@ -0,0 +1,18 @@
+package com.hontail.ui.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hontail.R
+import com.hontail.base.BaseFragment
+import com.hontail.databinding.FragmentProfileBinding
+import com.hontail.databinding.FragmentZzimBinding
+
+class ProfileFragment : BaseFragment<FragmentProfileBinding>(
+    FragmentProfileBinding::bind,
+    R.layout.fragment_profile
+) {
+
+}
---
+++ Hontail/app/src/main/java/com/hontail/ui/zzim/ZzimFragment.kt
@@ -0,0 +1,18 @@
+package com.hontail.ui.zzim
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hontail.R
+import com.hontail.base.BaseFragment
+import com.hontail.databinding.FragmentCocktailSearchBinding
+import com.hontail.databinding.FragmentZzimBinding
+
+class ZzimFragment : BaseFragment<FragmentZzimBinding>(
+    FragmentZzimBinding::bind,
+    R.layout.fragment_zzim
+) {
+
+}
