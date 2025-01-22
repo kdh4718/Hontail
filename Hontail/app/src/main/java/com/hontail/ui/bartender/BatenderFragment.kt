bab32636df69770c9a9c1417db7f49aca3938651
DESKTOP-PHKCE21\SSAFY <charlie216891@gmail.com>
2025-01-22 14:40:18 +0900

    [Feat] Add BatenderFragment #front

---
+++ Hontail/app/src/main/java/com/hontail/ui/bartender/BatenderFragment.kt
@@ -0,0 +1,12 @@
+package com.hontail.ui.bartender
+
+import com.hontail.R
+import com.hontail.base.BaseFragment
+import com.hontail.databinding.FragmentBatenderBinding
+
+class BatenderFragment : BaseFragment<FragmentBatenderBinding>(
+    FragmentBatenderBinding::bind,
+    R.layout.fragment_batender
+) {
+
+}
