96c2c31ff1b3bc588b4f0c1f4d9238320ca67afa
DESKTOP-PHKCE21\SSAFY <charlie216891@gmail.com>
2025-01-26 13:14:50 +0900

    [Style] Complement the Design FilterBottomSheetFragment  #front

--- Hontail/app/src/main/java/com/hontail/ui/picture/FilterBottomSheetFragment.kt
+++ Hontail/app/src/main/java/com/hontail/ui/picture/FilterBottomSheetFragment.kt
@@ -22,6 +22,41 @@ class FilterBottomSheetFragment : BottomSheetDialogFragment() {
         return binding?.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initRadio()
+    }
+
+    fun initRadio() {
+        binding?.let { binding ->
+            // 라디오 버튼 리스트
+            val radioButtons = listOf(
+                binding.radioButtonFilterBaseJin,
+                binding.radioButtonFilterBaseRum,
+                binding.radioButtonFilterBaseVodka,
+                binding.radioButtonFilterBaseWhiskey,
+                binding.radioButtonFilterBaseTequila,
+                binding.radioButtonFilterBaseLiqueur,
+                binding.radioButtonFilterBaseWine,
+                binding.radioButtonFilterBaseBrandy,
+                binding.radioButtonFilterBaseEtc
+            )
+
+            // 각 버튼 클릭 리스너 설정
+            radioButtons.forEach { radioButton ->
+                radioButton.setOnClickListener {
+                    // 클릭된 버튼을 제외한 나머지 버튼 선택 해제
+                    radioButtons.forEach { otherButton ->
+                        if (otherButton != radioButton) {
+                            otherButton.isChecked = false
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null  // 메모리 누수 방지를 위한 바인딩 객체 해제
