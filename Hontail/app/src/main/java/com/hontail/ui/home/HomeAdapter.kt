ed34c8e4d232ecd4d2c08ed7689b77bb0da3a0e6
DESKTOP-PHKCE21\SSAFY <charlie216891@gmail.com>
2025-02-04 16:16:26 +0900

    [Feat] Apply multi-view type RecyclerView HomeFragment #front

---
+++ Hontail/app/src/main/java/com/hontail/ui/home/HomeAdapter.kt
@@ -0,0 +1,108 @@
+package com.hontail.ui.home
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
+import androidx.recyclerview.widget.RecyclerView
+import com.hontail.R
+import com.hontail.databinding.ListItemHomeCategoryBinding
+import com.hontail.databinding.ListItemHomePictureDescriptionBinding
+import com.hontail.databinding.ListItemHomeToptenBinding
+import com.hontail.ui.MainActivity
+import com.hontail.util.CommonUtils
+
+class HomeAdapter(private val context: Context, private val items: List<HomeItem>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private val mainActivity = context as MainActivity
+
+    override fun getItemViewType(position: Int): Int {
+        return when (items[position]) {
+            is HomeItem.PictureDescription -> VIEW_TYPE_PICTURE_DESCRIPTION
+            is HomeItem.Category -> VIEW_TYPE_CATEGORY
+            is HomeItem.TopTen -> VIEW_TYPE_TOPTEN
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            VIEW_TYPE_PICTURE_DESCRIPTION -> {
+                val binding = ListItemHomePictureDescriptionBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                HomePictureDescriptionViewHolder(binding)
+            }
+
+            VIEW_TYPE_CATEGORY -> {
+                val binding = ListItemHomeCategoryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                HomeCategoryViewHolder(binding)
+            }
+
+            VIEW_TYPE_TOPTEN -> {
+                val binding = ListItemHomeToptenBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                HomeTopTenViewHolder(binding)
+            }
+
+            else -> throw IllegalArgumentException("Invalid view type")
+        }
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (val item = items[position]) {
+            is HomeItem.PictureDescription -> (holder as HomePictureDescriptionViewHolder).bind()
+            is HomeItem.Category -> (holder as HomeCategoryViewHolder).bind(item.categoryList)
+            is HomeItem.TopTen -> (holder as HomeTopTenViewHolder).bind(item.topTenList)
+        }
+    }
+
+
+    inner class HomePictureDescriptionViewHolder(private val binding: ListItemHomePictureDescriptionBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind() {
+            binding.textViewHomePictureBig.text = CommonUtils.changeTextColor(
+                binding.root.context,
+                "지금 당신을 위한\n완벽한 레시피를\n추천해드릴게요!",
+                "완벽한 레시피",
+                R.color.basic_pink
+            )
+
+            binding.imageViewHomePicture.setOnClickListener {
+                mainActivity.changeFragment(
+                    CommonUtils.MainFragmentName.COCKTAIL_TAKE_PICTURE_FRAGMENT
+                )
+            }
+        }
+    }
+
+    inner class HomeCategoryViewHolder(private val binding: ListItemHomeCategoryBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(categoryList: List<HomeCategoryItem>) {
+            val adapter = BaseCategoryAdapter(binding.root.context, categoryList)
+            binding.recyclerViewHomeCategory.layoutManager = GridLayoutManager(binding.root.context, 3)
+            binding.recyclerViewHomeCategory.adapter = adapter
+        }
+    }
+
+    inner class HomeTopTenViewHolder(private val binding: ListItemHomeToptenBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(topTenList: List<HomeTopTenItem>) {
+            val adapter = TopTenAdapter(binding.root.context, topTenList)
+            binding.recyclerViewHomeTopTen.layoutManager = SlowScrollLinearLayoutManager(binding.root.context)
+            binding.recyclerViewHomeTopTen.adapter = adapter
+            binding.recyclerViewHomeTopTen.addOnScrollListener(CenterScrollListener())
+
+            val startPosition = topTenList.size * 100 // 리스트의 중앙으로 설정
+            binding.recyclerViewHomeTopTen.scrollToPosition(startPosition)
+
+            val pagerSnapHelper = PagerSnapHelper()
+            pagerSnapHelper.attachToRecyclerView(binding.recyclerViewHomeTopTen)
+        }
+    }
+
+
+    companion object {
+        const val VIEW_TYPE_PICTURE_DESCRIPTION = 0
+        const val VIEW_TYPE_CATEGORY = 1
+        const val VIEW_TYPE_TOPTEN = 2
+    }
+}
--- Hontail/app/src/main/java/com/hontail/ui/home/HomeFragment.kt
+++ Hontail/app/src/main/java/com/hontail/ui/home/HomeFragment.kt
@@ -11,6 +11,7 @@ import android.view.View
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.PagerSnapHelper
 import com.hontail.R
 import com.hontail.base.BaseFragment
@@ -29,8 +30,7 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(
     private lateinit var mainActivity: MainActivity
     private val activityViewModel: MainActivityViewModel by activityViewModels()
 
-    private lateinit var topTenAdapter: TopTenAdapter
-    private lateinit var baseCategoryAdapter: BaseCategoryAdapter
+    private lateinit var homeAdapter: HomeAdapter
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -40,91 +40,47 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initAdapter()
-        initText()
-        initEvent()
     }
 
     fun initAdapter() {
-        val recyclerViewTopTen = binding.recyclerViewHomeTopTen
-
-        val items = listOf(
-            HomeTopTenItem(1, "네그로니\n스발리아토", R.drawable.topten_sample),
-            HomeTopTenItem(2, "마가리타", R.drawable.topten_sample),
-            HomeTopTenItem(3, "모히또", R.drawable.topten_sample),
-            HomeTopTenItem(4, "다이퀴리", R.drawable.topten_sample),
-            HomeTopTenItem(5, "마티니", R.drawable.topten_sample),
-            HomeTopTenItem(6, "올드 패션드", R.drawable.topten_sample),
-            HomeTopTenItem(7, "롱 아일랜드 아이스티", R.drawable.topten_sample),
-            HomeTopTenItem(8, "진토닉", R.drawable.topten_sample),
-            HomeTopTenItem(9, "위스키 사워", R.drawable.topten_sample),
-            HomeTopTenItem(10, "블러디 메리", R.drawable.topten_sample)
-        )
-
-        // 초기 스크롤 위치를 리스트 중앙으로 설정
-        recyclerViewTopTen.layoutManager = SlowScrollLinearLayoutManager(requireContext())
-        topTenAdapter = TopTenAdapter(requireContext(), items)
-        recyclerViewTopTen.adapter = topTenAdapter
-
-        val startPosition = items.size * 100 // 시작 위치를 중간으로 설정
-        recyclerViewTopTen.scrollToPosition(startPosition)
-
-        recyclerViewTopTen.addOnScrollListener(CenterScrollListener()) // 중앙 확대 효과 적용
-
-        val pagerSnapHelper = PagerSnapHelper()
-        pagerSnapHelper.attachToRecyclerView(recyclerViewTopTen)
-
-        val recyclerViewBaseCategory = binding.recyclerViewHomeCategory
-
-        val categoryItems = listOf(
-            HomeCategoryItem("진", "허브와 시트러스 향", R.drawable.category_jin),
-            HomeCategoryItem("럼", "사탕수수의 달콤함", R.drawable.category_rum),
-            HomeCategoryItem("보드카", "중립적이고 깨끗한 맛", R.drawable.category_vodka),
-            HomeCategoryItem("위스키", "깊고 진한 풍미", R.drawable.category_whiskey),
-            HomeCategoryItem("데킬라", "선명하고 강렬한 맛", R.drawable.category_tequila),
-            HomeCategoryItem("리큐어", "과일, 허브 ,향신료", R.drawable.category_liqueur),
-            HomeCategoryItem("와인", "부드러운 여운", R.drawable.category_wine),
-            HomeCategoryItem("브랜디", "부드러운 목넘김", R.drawable.category_brandy),
-            HomeCategoryItem("기타", "K-술, 무알콜 등", R.drawable.category_etc)
-        )
-
-        val gridLayoutManager = GridLayoutManager(requireContext(), 3)
-        gridLayoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
-            override fun getSpanSize(position: Int): Int {
-                return 1 // 모든 아이템 동일한 크기 유지
-            }
-        }
-
-        baseCategoryAdapter = BaseCategoryAdapter(requireContext(), categoryItems)
-
-        // RecyclerView 설정
-        recyclerViewBaseCategory.apply {
-            layoutManager = gridLayoutManager
-            adapter = baseCategoryAdapter
-            setHasFixedSize(true) // 성능 최적화
-        }
-        recyclerViewBaseCategory.adapter = baseCategoryAdapter
-    }
-
-    fun initText() {
         binding.apply {
-            textViewHomePictureBig.text =
-                CommonUtils.changeTextColor(requireContext(), "지금 당신을 위한\n완벽한 레시피를\n추천해드릴게요!", "완벽한 레시피", R.color.basic_pink)
+            val categoryItems = listOf(
+                HomeCategoryItem("진", "허브와 시트러스 향", R.drawable.category_jin),
+                HomeCategoryItem("럼", "사탕수수의 달콤함", R.drawable.category_rum),
+                HomeCategoryItem("보드카", "중립적이고 깨끗한 맛", R.drawable.category_vodka),
+                HomeCategoryItem("위스키", "깊고 진한 풍미", R.drawable.category_whiskey),
+                HomeCategoryItem("데킬라", "선명하고 강렬한 맛", R.drawable.category_tequila),
+                HomeCategoryItem("리큐어", "과일, 허브, 향신료", R.drawable.category_liqueur),
+                HomeCategoryItem("와인", "과일향의 여운", R.drawable.category_wine),
+                HomeCategoryItem("브랜디", "부드러운 목넘김", R.drawable.category_brandy),
+                HomeCategoryItem("기타", "K-술, 무알콜 등", R.drawable.category_etc),
 
-            textViewHomeCategory.text =
-                CommonUtils.changeTextColor(requireContext(), "칵테일 초이스가 어렵다면\n베이스부터 골라보는 건 어떨까요?", "베이스", R.color.basic_sky)
+                )
 
-            textViewHomeTopTen.text =
-                CommonUtils.changeTextColor(requireContext(), "가장 많은 사랑을 받은\n인기 칵테일 TOP 10!", "TOP 10!", R.color.basic_pink)
-        }
-    }
+            val topTenItems = listOf(
+                HomeTopTenItem(1, "네그로니\n스발리아토", R.drawable.topten_sample),
+                HomeTopTenItem(2, "마가리타", R.drawable.topten_sample),
+                HomeTopTenItem(3, "모히또", R.drawable.topten_sample)
+            )
 
-    fun initEvent() {
-        binding.apply {
-            imageViewHomePicture.setOnClickListener {
-                mainActivity.changeFragment(CommonUtils.MainFragmentName.COCKTAIL_TAKE_PICTURE_FRAGMENT)
+            val items = mutableListOf<HomeItem>().apply {
+                add(HomeItem.PictureDescription)
+                add(HomeItem.Category(categoryItems))
+                add(HomeItem.TopTen(topTenItems))
             }
+
+            homeAdapter = HomeAdapter(requireContext(), items)
+            recyclerViewHome.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+            recyclerViewHome.adapter = homeAdapter
         }
+
     }
+}
+
+sealed class HomeItem {
+    object PictureDescription : HomeItem()
+    data class Category(val categoryList: List<HomeCategoryItem>) : HomeItem()
+    data class TopTen(val topTenList: List<HomeTopTenItem>) : HomeItem()
 }
 
 data class HomeTopTenItem(
