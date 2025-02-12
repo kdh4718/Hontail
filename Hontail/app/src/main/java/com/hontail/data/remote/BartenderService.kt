c9f66f757d64ac4c8e8ba9cf9326577813b32cac
hyuun <wotj28@naver.com>
2025-02-12 16:25:46 +0900

    [Fix] Fix BartenderService #front

--- Hontail/app/src/main/java/com/hontail/data/remote/BartenderService.kt
+++ Hontail/app/src/main/java/com/hontail/data/remote/BartenderService.kt
@@ -12,13 +12,10 @@ interface BartenderService {
     // 바텐더한테 메시지 보내기.
     @POST("api/bartender/chat")
     suspend fun sendToBartender(
-        @Body userMessage: String,
+        @Body userMessage: String
     ): Response<BartenderResponse>
 
     // 바텐더로부터 초기 인삿말 받기.
     @GET("api/bartender/greeting")
-    suspend fun receiveFromBartender(
-        @Query("userId") userId: Int,
-        @Query("nickname") nickname: String
-    ): Response<BartenderResponse>
+    suspend fun receiveFromBartender(): Response<BartenderResponse>
 }
