430fe769283a4459eb1951da3570887d8ef4d345
DESKTOP-4QCTMKG\SSAFY <aash711@naver.com>
2025-02-12 16:15:44 +0900

    [Feat] Update cocktail liked API to include recent viewed cocktails

--- Hontail_Back/src/main/java/com/hontail/back/cocktail/service/CocktailService.java
+++ Hontail_Back/src/main/java/com/hontail/back/cocktail/service/CocktailService.java
@@ -2,6 +2,7 @@ package com.hontail.back.cocktail.service;
 
 import com.hontail.back.cocktail.dto.CocktailSummaryDto;
 import com.hontail.back.cocktail.dto.TopLikedCocktailDto;
+import com.hontail.back.cocktail.dto.UserCocktailResponseDto;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -15,11 +16,14 @@ public interface CocktailService {
             @RequestParam(required = false) String baseSpirit,
             int page, int size, boolean isCustom, Integer userId);
 
-    List<TopLikedCocktailDto> getTopLikedCocktails();
+    List<TopLikedCocktailDto> getTopLikedCocktails(); // 사용자가 좋아요 한 칵테일 (사용하지 않으나 혹시 몰라 남겨둠)
 
     List<CocktailSummaryDto> getLikedCocktails(Integer userId);
 
     Page<CocktailSummaryDto> searchCocktails(String keyword, int page, int size, Integer userId);
 
     Page<CocktailSummaryDto> getMyCocktails(Integer userId, int page, int size); // 나만의 칵테일 리스트
+
+    // 사용자가 좋아요 한 칵테일과 최근 본 칵테일 목록
+    UserCocktailResponseDto getLikedAndRecentCocktails(Integer userId, List<Integer> recentCocktailIds);
 }
