199f00b1a8be1b8ca3e451dc32aa91bd581eef39
cosmos2123 <cosmos2123@naver.com>
2025-02-13 16:01:29 +0900

    [Fix] emergency login error #back

---
+++ Hontail_Back/src/main/java/com/hontail/back/db/entity/AccessToken.java
@@ -0,0 +1,38 @@
+package com.hontail.back.db.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "access_tokens")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccessToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private int userId;
+
+    @Column(columnDefinition = "VARCHAR(2048)", nullable = false)
+    private String token;
+
+    @Column(name = "expiry_date", nullable = false)
+    private LocalDateTime expiryDate;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
