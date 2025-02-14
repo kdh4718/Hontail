f217e57fe6e4c2995e35fc6a4f8fb1a5e8df3b6a
김두영 <dubblx05@naver.com>
2025-02-14 15:00:27 +0900

    [Fix] Fix swagger issue

--- Hontail_Back/src/main/java/com/hontail/back/config/SwaggerConfig.java
+++ Hontail_Back/src/main/java/com/hontail/back/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.hontail.back.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,10 +12,19 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        String schemeName = "bearerAuth";
+
         return new OpenAPI()
                 .info(new Info()
                         .title("혼테일 API")
                         .description("혼테일 프로젝트 API 명세서")
-                        .version("v1.0.0"));
+                        .version("v1.0.0"))
+                .addSecurityItem(new SecurityRequirement().addList(schemeName)) // 보안 요구 사항 추가
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes(schemeName, new SecurityScheme()
+                                .name(schemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT"))); // Bearer 토큰 인증 방식 설정
     }
 }
